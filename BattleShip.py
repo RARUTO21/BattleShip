7d7575015fd382ea0183b1cce5065e987b0605e8
Anthony <Anthonylle@hotmail.com>
2016-09-22 22:09:31 -0600

    Funcion de contar dígitos añadida

--- BattleShip.py
+++ BattleShip.py
@@ -1,6 +1,11 @@
 
-print "hola anthony playo"
+print ("hola anthony playo")
 
 def funcion():
     return 1
 
+
+def contarDigitos(num):
+    if num== 0:
+        return 0
+    return 1+contarDigitos(num//10)
