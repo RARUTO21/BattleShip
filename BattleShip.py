5e2767f4a9c9e17175d5f731a16e4334a6d572b8
Ricardo <ricardocastrom06@gmail.com>
2016-10-02 21:50:40 -0600

    Hilos y archivos

--- BattleShip.py
+++ BattleShip.py
@@ -1,6 +1,9 @@
 from tkinter.filedialog import *
 from tkinter import *
-
+import random
+import threading
+import time
+matriz=[]# para guardar los datos cuando se cargue el archivo
 nombreJugador = ""
 
 
@@ -15,6 +18,7 @@ ventanaPrincipal.title("BattleShip")
 ventanaPrincipal.geometry("1280x768")
 ventanaPrincipal.config(bg="gray25")
 ventanaPrincipal.resizable(0,0)
+
 
 #=====================================
 
@@ -38,6 +42,8 @@ fondoPrincipal = PhotoImage(file = "Images/Battleship.gif")
 #                 Labels
 lblFondoVentanaPrincipal = Label(ventanaPrincipal,image=fondoPrincipal,bg="black")
 lblFondoVentanaPrincipal.place(x=0,y=0)
+lblCronometro=Label(height=3,width=10,bg="white")
+lblCronometro.place(x=10,y=10)
 #===========================================================Labels
 
 
@@ -46,10 +52,58 @@ lblFondoVentanaPrincipal.place(x=0,y=0)
 
 
 #==========================================================Botones
+#==========================================================Archivos
+def agregar(linea):
+    linea= linea.split()
+    #validar que los datos de la matriz sean los que queremos
+    matriz.append(linea)
+    
+def cargarArchivo():
+  # hay que crear los archivos xD pero la vara si agarra las cosas de un archivo
+    lista=["Archivo1.txt","Archivo2.txt","Archivo3.txt","Archivo4.txt"]
+    random.shuffle(lista)
+    archivo = open(lista[0], "r")
+    for linea in archivo.readlines():
+        agregar (linea)
+    print (matriz)
+#==========================================================
+#==========================================================Archivos
+def agregar(linea):
+    linea= linea.split()
+    #validar que los datos de la matriz sean los que queremos
+    matriz.append(linea)
+    
+def cargarArchivo():
+  # hay que crear los archivos xD pero la vara si agarra las cosas de un archivo
+    lista=["Archivo1.txt","Archivo2.txt","Archivo3.txt","Archivo4.txt"]
+    random.shuffle(lista)
+    archivo = open(lista[0], "r")
+    for linea in archivo.readlines():
+        agregar (linea)
+    print (matriz)
+#==========================================================
 
-
+        
+def Cronometro():
+    """funcion que realiza el trabajo en el thread"""
+    contador=0
+    while contador < 10 :
+        contador= contador+1
+        time.sleep(1)
+        lblCronometro.config(text=str(contador))
+        
+        print (contador)
+        
 
 # ___________ Finalmente ___________
 
-iniciar()
+Crono = threading.Thread(target=Cronometro)
+Crono.start()
+Inicio=threading.Thread(target=iniciar)
+Inicio.start()
 ventanaPrincipal.mainloop()
+Crono.join()
+Inicio.join()
+
+
+
