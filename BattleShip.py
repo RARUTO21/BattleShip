7ebe7b7b2c89630bf0a3f44bb6504d0c6c7c57da
Ricardo <ricardocastrom06@gmail.com>
2016-10-28 10:47:35 -0600

    arregle algunas cosas y puse guardar partida

--- BattleShip.py
+++ BattleShip.py
@@ -3,8 +3,12 @@ from tkinter import *
 import random
 import threading
 import time
-matriz=[]# para guardar los datos cuando se cargue el archivo
+
 nombreJugador = ""
+TiempoJuego=0
+MatrizUsuario=[['0','0','0','0','0','0','0','0','0','0'],['0','0','0','0','0','0','0','0','0','0'],['0','0','0','0','0','0','0','0','0','0'],['0','0','0','0','0','0','0','0','0','0'],['0','0','0','0','0','0','0','0','0','0'],['0','0','0','0','0','0','0','0','0','0'],['0','0','0','0','0','0','0','0','0','0'],['0','0','0','0','0','0','0','0','0','0'],['0','0','0','0','0','0','0','0','0','0'],['0','0','0','0','0','0','0','0','0','0']]
+MatrizCompu=[]
+numeroBarco=1
 
 
 def iniciar():
@@ -56,50 +60,61 @@ lblCronometro.place(x=10,y=10)
 def agregar(linea):
     linea= linea.split()
     #validar que los datos de la matriz sean los que queremos
-    matriz.append(linea)
+    MatrizCompu.append(linea)
     
 def cargarArchivo():
-  # hay que crear los archivos xD pero la vara si agarra las cosas de un archivo
-    lista=["Archivo1.txt","Archivo2.txt","Archivo3.txt","Archivo4.txt"]
-    random.shuffle(lista)
-    archivo = open(lista[0], "r")
-    for linea in archivo.readlines():
-        agregar (linea)
-    print (matriz)
-#==========================================================
-#==========================================================Archivos
-def agregar(linea):
-    linea= linea.split()
-    #validar que los datos de la matriz sean los que queremos
-    matriz.append(linea)
+    global tiempo
+    global MatrizCompu
+    #lista=["Archivo1.txt","Archivo2.txt","Archivo3.txt","Archivo4.txt"]
+    #random.shuffle(lista)
+   # print (lista[0])
+    archivo = open("ricardo.txt", "r")
+    tiempo=int(archivo.readline(2))
     
-def cargarArchivo():
-  # hay que crear los archivos xD pero la vara si agarra las cosas de un archivo
-    lista=["Archivo1.txt","Archivo2.txt","Archivo3.txt","Archivo4.txt"]
-    random.shuffle(lista)
-    archivo = open(lista[0], "r")
     for linea in archivo.readlines():
         agregar (linea)
-    print (matriz)
+    MatrizCompu=MatrizCompu[1:]
+    print(MatrizCompu)
+   # escribir(matriz)
+
+def Guardar(lista):
+    limite = len(lista)
+    contador=0
+    outfile = open('ricardo.txt', 'w') # Indicamos el valor 'w'.
+    outfile.write(str(TiempoJuego))
+    outfile.write("\n")
+    while (contador<limite):
+         columnas=len(lista[0])-1
+         contador2=0
+         
+         while (contador2<columnas):
+             outfile.write(str(lista[contador][contador2])+" ")
+
+             contador2=contador2+1
+         outfile.write(str(lista[contador][contador2]))
+         outfile.write("\n")
+         contador=contador+1
+    
+    outfile.close()
 #==========================================================
 
         
 def Cronometro():
     """funcion que realiza el trabajo en el thread"""
-    contador=0
-    while contador < 10 :
+    TiempoJuego=0
+    while TiempoJuego < 10 :
         contador= contador+1
         time.sleep(1)
-        lblCronometro.config(text=str(contador))
+        lblCronometro.config(text=str(TiempoJuego))
         
-        print (contador)
+        print (TiempoJuego)
 #==========================================================
 
 def PosicionarBarco(x,y,tamaño,orientacion):
    
     if(orientacion=="H"):
-        if ((10-y)>=tamaño):#pregunta si el barco cabe en el limite Horizontal
-            if(cabe(x,y,tamaño,orientacion)):#si el barco cabe sin chocar con otro lo asigna
+        if ((10-y)>=tamaño):
+            if(cabe(x,y,tamaño,orientacion)):
                 asignar(x,y,tamaño,orientacion)
                 return "asignado correctamente"
             else:
@@ -107,8 +122,8 @@ def PosicionarBarco(x,y,tamaño,orientacion):
         else:
             return "error, no cabe"
     else:
-        if((10-x)>=tamaño):#pregunta si el barco cabe en el limite vertical
-            if(cabe(x,y,tamaño,orientacion)):#si el barco cabe sin chocar con otro lo asigna
+        if((10-x)>=tamaño):
+            if(cabe(x,y,tamaño,orientacion)):
                 asignar(x,y,tamaño,orientacion)
                 
                 return "asignado correctamente"
@@ -118,60 +133,176 @@ def PosicionarBarco(x,y,tamaño,orientacion):
             return "error, no cabe"
 
 def cabe(x,y,tamaño,orientacion):
+    y2=y
     if(orientacion=="H"):
-        contador=0#busca en la matriz X=tamaño cantidad de lugares disponibles para asignar Horizontal
+        contador=0
         while(contador<tamaño):
-            if(matriz[x][y]=='0'):
-                y=y+1#si encuentra agua entonces sigue sigue buscando disponible
+            if(MatrizUsuario[x][y]=='0'):
+                y=y+1
                 contador=contador+1
                 print(contador)
-            else:#si encontró uno ocupado entonces sale del ciclo
-                break
-        if(contador==tamaño):
-            return True# si termina el ciclo es porque hay campo para asignar y devuelve True
-        else:# si no hay campo devuelve False
-            return False
+            else:
+                 return False
+        
+        return bordesH(x,y2,tamaño)
     else:
-        contador=0#busca en la matriz X=tamaño cantidad de lugares disponibles para asignar Vertical
+        x2=x
+        contador=0
         while(contador<tamaño):
-            if(matriz[x][y]=='0'):
-                x=x+1#si encuentra agua entonces sigue sigue buscando disponible
+            if(MatrizUsuario[x][y]=='0'):
+                x=x+1
                 contador=contador+1
             else:
-                break#si encontró uno ocupado entonces sale del ciclo
-        if(contador==tamaño):
-            return True# si termina el ciclo es porque hay campo para asignar y devuelve True
-        else:# si no hay campo devuelve False
+                return False
+        
+        return bordesV(x2,y,tamaño)
+
+def bordesH(x,y,tamaño):
+    y2=y
+    if(x==0):# si es igual que 0 solo valida la linea 2
+        vacio=0
+        while(vacio<tamaño):
+            if(MatrizUsuario[x+1][y]=='0'):
+                vacio=vacio+1
+                y=y+1
+                print("while")
+            else:
+                return False
+        
+    elif(x==9): # si es igual que 9 solo valida la 8
+        vacio=0
+        while(vacio<tamaño):
+            if(MatrizUsuario[x-1][y]=='0'):
+                vacio=vacio+1
+                y=y+1
+            else:
+                return False
+    else:
+        vacio=0
+        while(vacio<tamaño):
+            if(MatrizUsuario[x-1][y]=='0' and MatrizUsuario[x+1][y]=='0'):
+                vacio=vacio+1
+                y=y+1
+            else:
+                return False
+        
+    if(y2==0):
+        if(x==0):
+            if(MatrizUsuario[x][tamaño]=='0' and MatrizUsuario[x+1][tamaño]=='0'):
+                return True
             return False
+        elif(x==9):
+            if(MatrizUsuario[x][tamaño]=='0' and MatrizUsuario[x-1][tamaño]=='0'):
+                return True
+            return False
+        else:
+            if(MatrizUsuario[x][tamaño]=='0' and MatrizUsuario[x-1][tamaño]=='0' and MatrizUsuario[x+1][tamaño]=='0'):
+                return True
+            return False
+    elif(y2==9):
+        if(x==0):
+            if(MatrizUsuario[x][8]=='0' and MatrizUsuario[x+1][8]=='0'):
+                return True
+            return False
+        elif(x==9):
+            if(MatrizUsuario[x][8]=='0' and MatrizUsuario[x-1][8]=='0'):
+                return True
+            return False
+        else:
+            if(MatrizUsuario[x][8]=='0' and MatrizUsuario[x-1][8]=='0' and MatrizUsuario[x+1][8]=='0'):
+                return True
+            return False
+    else:
+        if(y2+tamaño>9):
+            if(MatrizUsuario[x-1][y2-1]=='0' and MatrizUsuario[x][y2]=='0' and MatrizUsuario[x-1][y2+tamaño-1]=='0'):
+                return True
+        else:
+            if(MatrizUsuario[x][y2]=='0' and MatrizUsuario[x][y2-1]=='0' and MatrizUsuario[x][y2+tamaño]=='0' and MatrizUsuario[x-1][y2+tamaño]=='0'and MatrizUsuario[x+1][y2+tamaño]=='0'):
+                return True
+            
+        return False
+def bordesV(x,y,tamaño):
+    x2=x
+    if(y==0):
+        vacio=0
+        while(vacio<tamaño):
+            if(MatrizUsuario[x][y+1]=='0'):
+                vacio=vacio+1
+                x=x+1
+                print("while")
+            else:
+                return False
+        
+    elif(y==9):
+        vacio=0
+        x=x-1
+        while(vacio<=tamaño):
+            if(MatrizUsuario[x][y-1]=='0'):
+                vacio=vacio+1
+                x=x+1
+            else:
+                return False
+    else:
+        vacio=0
+        while(vacio<tamaño):
+            if(MatrizUsuario[x][y-1]=='0' and MatrizUsuario[x][y+1]=='0'):
+                vacio=vacio+1
+                x=x+1
+            else:
+                return False
+        
+    if(x2==0):
+        print(MatrizUsuario[x][tamaño])
+        if(MatrizUsuario[tamaño][y]=='0'):
+            return True
+        return False
+    elif(x2==9):
+        if(MatrizUsuario[x2][-(tamaño+1)]=='0'):
+            return True
+        return False
+    else:
+        if(x2+tamaño>9):
+            if(MatrizUsuario[x2-1][y]=='0' and MatrizUsuario[x2+tamaño-1][y]=='0'):
+                return True
+        else:
+            if(MatrizUsuario[x2-1][y]=='0' and MatrizUsuario[x2-1][y-1]=='0' and MatrizUsuario[x2-1][y+1]=='0' and MatrizUsuario[x2+tamaño][y]=='0'and MatrizUsuario[x2+tamaño][y-1]=='0'and MatrizUsuario[x2+tamaño][y+1]=='0'):
+                return True
+        return False
+        
+        
+    
+    
+             
+
 
 def asignar(x,y,tamaño,orientacion):
     global numeroBarco
     if(orientacion=="H"):
         contador=0
-        while(contador<tamaño):#asigna los lugares x,y desde el punto establecido hasta el tamaño del barco horizontal
-            matriz[x][y]=str(numeroBarco)
+        while(contador<tamaño):
+            MatrizUsuario[x][y]=str(numeroBarco)
             y=y+1
             contador=contador+1
             print(contador)
            
     else:
         contador=0
-        while(contador<tamaño):#asigna los lugares x,y desde el punto establecido hasta el tamaño del barco vertical
-            matriz[x][y]=str(numeroBarco)
+        while(contador<tamaño):
+            MatrizUsuario[x][y]=str(numeroBarco)
             x=x+1
             contador=contador+1
-    numeroBarco=numeroBarco+1#aumenta el numero de barcos ya que al asignarlos se les pone el numero de barco por el cual va este contador
+    numeroBarco=numeroBarco+1
 
 
 
 def pegar(matriz,x,y):
-    if(matriz[x][y]=='0'):#si le pega a un 0 es agua
+    if(matriz[x][y]=='0'):
         return "agua"
-    elif(destruido(matriz,x,y)):#si destruye el barco pone un 0 en esa posición e indica que ha sido destruido
+    elif(destruido(matriz,x,y)):
         matriz[x][y]='0'
         return "destruido"
     else:
-        matriz[x][y]='0'# si solo pega al barco indica que ha sudo pegado y pone un 0 en esa posicion
+        matriz[x][y]='0'
         return "pegado"
 def destruido(matriz,x,y):
     dato=matriz[x][y]
@@ -180,19 +311,20 @@ def destruido(matriz,x,y):
     columnas=10
     contadorFilas=0
     encontrado=0
-    while(contadorFilas<filas):#revisa toda la matriz en busca de un numero igual al de barco acertado
+    while(contadorFilas<filas):
         contadorColumnas=0
         while(contadorColumnas<columnas):
             if(matriz[contadorFilas][contadorColumnas]==dato):
-                encontrado=encontrado+1#si lo encuentra suma 1 a encontrados
+                encontrado=encontrado+1
             
             contadorColumnas=contadorColumnas+1
         
         contadorFilas=contadorFilas+1
-    if(encontrado>1):# si encuentra mas de 1 entonces el barco no ha sido destruido
+    if(encontrado>1):
         return False
-    else:# si encuentra 1 lo destruye
+    else:
         return True
+        
         
 #==========================================================
 
@@ -205,6 +337,8 @@ Inicio.start()
 ventanaPrincipal.mainloop()
 Crono.join()
 Inicio.join()
+
+
 
 
 
